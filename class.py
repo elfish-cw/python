e68957229fa5c86d50e2ee3374dab60405786764
Lana <elfish_cw@mail.ru>
2023-04-27 15:59:14 +0300

    дорешала классную задачу со словарем

--- class.py
+++ class.py
@@ -60,10 +60,11 @@
 a = [{"V": "S001"}, {"V": "S002"},{"VI": "S001"}, {"VI": "S005"}, {"VII": "S005"}, {"V":"S009"}, {"VIII":"S007"}]
 d = []
 for i in a:
-    for v in i:
-        d.append(i[v])
+    for m in i:
+        d.append(i[m])
 
 print(set(d))
+
 
 
 
